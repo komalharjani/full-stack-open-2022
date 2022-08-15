968c7c366af7cd6e9af1d5bcede4659c832b9ad2
komalharjani <komaalharjani@gmail.com>
2022-08-15 14:47:57 +0100

    Exercise 1.5

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,6 +1,7 @@
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
     {
       name: 'Fundamentals of React',
       exercises: 10
@@ -14,13 +15,13 @@ const App = () => {
       exercises: 14
     }
   ]
-
+  }
 
     return (
       <div>
         <Header course={course} />
-        <Content parts={parts}/>
-        <Total  parts={parts}/>
+        <Content parts={course.parts}/>
+        <Total  parts={course.parts}/>
       </div>
     )
   }
@@ -30,7 +31,7 @@ const Header = (props) => {
   return (
     <div>
       <h1>
-        {props.course}.
+        {props.course.name}
       </h1>
     </div>
   )
